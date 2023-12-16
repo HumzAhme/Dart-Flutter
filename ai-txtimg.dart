7d784c9ae3a55335b1f64abaf5d72e611ddc44f2
Signal It! <127075542+HumzAhme@users.noreply.github.com>
2023-12-16 20:43:09 +0500

    Update ai-txtimg.dart
    rectangled image 'Inspirations' added
    responsive 'generate' button added with gradient color
    drop down STF widget code started

--- ai-txtimg.dart
+++ ai-txtimg.dart
@@ -9,6 +9,12 @@ import 'package:simple_gradient_text/simple_gradient_text.dart';
 //can only one thing widget like scaffold can be returned in one stl widget? yes.
 //text input border increase & text in lines as para. [solved] take Text() instead of textField().
 
+//The image on each widget called must be different sourced in realtime to that calling & not predefined inside widget.
+
+//- responsive ai writing;stful
+//- dropdown menu;stful
+//- all responsiv
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -209,6 +215,57 @@ class BtomNavBar extends StatelessWidget {
       ),
       navBarStyle:
           NavBarStyle.style1, // Choose the nav bar style with this property.
+    );
+  }
+}
+
+const List<String> list = <String>['1:1', '12:8', '16:9', 'HD'];
+String dropdownValue = list.first;
+
+// class drpDwn extends StatefulWidget {
+//   const drpDwn({super.key});
+
+//   @override
+//   State<drpDwn> createState() => drpDwn2();
+
+//   Widget build(BuildContext context) {
+//     return DropdownMenu<String>(
+//       initialSelection: list.first,
+//       onSelected: (String? value) {
+//         // This is called when the user selects an item.
+//         setState(() {
+//           dropdownValue = value!;
+//         });
+//       },
+//       dropdownMenuEntries: list.map<DropdownMenuEntry<String>>((String value) {
+//         return DropdownMenuEntry<String>(value: value, label: value);
+//       }).toList(),
+//     );
+//   }
+// }
+class DropdownMenuExample extends StatefulWidget {
+  const DropdownMenuExample({super.key});
+
+  @override
+  State<DropdownMenuExample> createState() => _DropdownMenuExampleState();
+}
+
+class _DropdownMenuExampleState extends State<DropdownMenuExample> {
+  String dropdownValue = list.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownMenu<String>(
+      initialSelection: list.first,
+      onSelected: (String? value) {
+        // This is called when the user selects an item.
+        setState(() {
+          dropdownValue = value!;
+        });
+      },
+      dropdownMenuEntries: list.map<DropdownMenuEntry<String>>((String value) {
+        return DropdownMenuEntry<String>(value: value, label: value);
+      }).toList(),
     );
   }
 }
@@ -452,7 +509,12 @@ class Screen1 extends StatelessWidget {
                       Icons.window_outlined,
                     ),
                     Text('1:1'),
-                    Icon(Icons.arrow_downward_rounded),
+                    IconButton(
+                      icon: Icon(Icons.arrow_downward_rounded),
+                      onPressed: () {
+                        DropdownMenuExample();
+                      },
+                    ),
                   ],
                 ),
               ),
@@ -590,6 +652,116 @@ class Screen1 extends StatelessWidget {
             ],
           ),
         ),
+        // Padding(
+        //   padding: const EdgeInsets.only(top: 18.0),
+        //   child: Divider(
+        //     color: Colors.white,
+        //     thickness: 6,
+        //   ),
+        // ),
+        Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Container(
+            width: 326,
+            height: 76,
+            padding: EdgeInsets.only(
+                top: 0,
+                left:
+                    0), //padding this separtes button with container edge by this padding
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.transparent,
+                shadowColor: Colors.transparent,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(27)),
+              ),
+              onPressed: () {},
+              child: Center(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.movie_filter,
+                        size: 31,
+                      ),
+                      Padding(padding: EdgeInsets.symmetric(horizontal: 6.5)),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Generate',
+                            style: GoogleFonts.delaGothicOne(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Text('Watch on AD')
+                        ],
+                      ),
+                    ]),
+              ),
+            ),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color.fromARGB(255, 243, 0, 150),
+                    Color.fromRGBO(227, 139, 115, 1),
+                  ],
+                ),
+                borderRadius: BorderRadius.circular(27)),
+          ),
+        ),
+        Container(
+            padding: EdgeInsets.only(top: 5, left: 18, bottom: 7),
+            child: Text(
+              'Inspirations',
+              style: TextStyle(fontWeight: FontWeight.w800, fontSize: 16),
+            )),
+        Container(
+          padding: EdgeInsets.only(left: 12, right: 12),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Rectimag(),
+                  Rectimag(),
+                ],
+              ),
+              Row(
+                children: [
+                  Rectimag(),
+                  Rectimag(),
+                ],
+              ),
+              Row(
+                children: [
+                  Rectimag(),
+                  Rectimag(),
+                ],
+              ),
+              Row(
+                children: [
+                  Rectimag(),
+                  Rectimag(),
+                ],
+              ),
+              Row(
+                children: [
+                  Rectimag(),
+                  Rectimag(),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Divider(
+          color: Colors.white,
+          thickness: 13,
+        ),
       ]),
     );
   }
@@ -617,7 +789,7 @@ class boximag extends StatelessWidget {
                     top: 0,
                   ),
                   child: Image.asset(
-                    'imagz/jkt2.jpg',
+                    'imgz/kor.jpg',
                     width: 270,
                     height: 324,
                     fit: BoxFit.cover,
@@ -636,6 +808,53 @@ class boximag extends StatelessWidget {
     ); //box-image container twrds right scroll
   }
 }
+
+class Rectimag extends StatelessWidget {
+  const Rectimag({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //box-image container twrds right scroll
+      padding: EdgeInsets.symmetric(horizontal: 13.7, vertical: 12.1),
+      alignment: Alignment.center,
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Container(
+              padding: const EdgeInsets.only(left: 0, top: 0),
+              height:
+                  200, //if not added, 'BoxConstraints(w=148.0, h=Infinity)' applies.
+              child: SizedBox.fromSize(
+                size: Size.fromWidth(148), //Size.fromWidth(58),
+                child: Container(
+                  padding: const EdgeInsets.only(
+                    left: 0,
+                    top: 0,
+                  ),
+                  child: Image.asset(
+                    'imgz/fns.jpg',
+                    width: 270,
+                    height: 324,
+                    fit: BoxFit.cover,
+                  ), //Image.network(
+                  //'$lnk', //https://i.pinimg.com/236x/b5/52/0b/b5520b156b33e9f3094ba307bc9d3904.jpg not working due to CORS
+                  //width: 100,
+                  //height: 100,
+                  //fit: BoxFit.cover,
+                  // ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ); //box-image container twrds right scroll
+  }
+}
+//inspiration image
+//
 
 class Screen2 extends StatelessWidget {
   const Screen2({super.key});
